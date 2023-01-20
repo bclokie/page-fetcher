b82b50c97d5be5b21717829d96e237d87fd67f02
Braden <braden.clokie@gmail.com>
2023-01-20 17:00:08 -0500

    Created page-fetcher function

---
+++ fetcher.js
@@ -0,0 +1,19 @@
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+    if (error) {
+        console.log(`Error: ${error}`);
+        return;
+    }
+    fs.writeFile(filePath, body, (err) => {
+        if (err) {
+            console.log(`Error: ${err}`);
+            return;
+        }
+        console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`);
+    });
+});
